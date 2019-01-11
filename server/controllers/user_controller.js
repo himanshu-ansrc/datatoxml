e9e3380b09f74647ee1d5c15a5f8496e1cb46ed7
himasnhu savita <himanshu131194@users.noreply.github.com>
2019-01-11 18:05:51 +0530

    changes
    changes

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -16,16 +16,25 @@ function replaceAll(str, term, replacement) {
   return str.replace(new RegExp(escapeRegExp(term), 'g'), replacement);
 }
 
+const parentTagsForXML = `<?xml version="1.0" encoding="UTF-8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">`;
+
+const closeParentTagsForXML = `</xs:schema>`;
+
 const dataToXml = (req, res)=>{
             const xmlToSave = crypto.randomBytes(16).toString('hex')+'.xml';
             let questionselementjson = req.body['dataList'];
+
             let arr = [];
             console.log(questionselementjson);
             for(let x in questionselementjson){
                 let questionElemnet = replaceAll(questionselementjson[x], '\n', '<\\n>');//questionselementjson[x].replaceAll("\n", "<br/>")
-                arr.push({ name : x, text: questionElemnet })
+                arr.push({ name : "", text: questionElemnet })
             }
-            const xml = jsontoxml({ record:arr});
+            let xml = jsontoxml({
+                  record : arr
+            });
+            xml = `${parentTagsForXML}${xml}${closeParentTagsForXML}`;
             fs.writeFile(`temp/${xmlToSave}`, xml, function (err){
                        if(err) throw err;
                        res.send({url : `/${xmlToSave}`})
